949ee29bffe11479f1f87c9ae7d93bc6fdd6ea48
Elves Aguiar <elvesmhs@hotmail.com>
2023-05-16 11:18:11 -0300

    feat: Entity order

---
+++ src/main/java/com/elves/dscommerce/entities/OrderStatus.java
@@ -0,0 +1,9 @@
+package com.elves.dscommerce.entities;
+
+public enum OrderStatus {
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERD,
+    CANCELED;
+}
--- src/main/java/com/elves/dscommerce/entities/User.java
+++ src/main/java/com/elves/dscommerce/entities/User.java
@@ -3,6 +3,8 @@ package com.elves.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -16,6 +18,9 @@ public class User {
     private String phone;
     private LocalDate birthDate;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User (){
 
@@ -78,5 +83,7 @@ public class User {
         this.password = password;
     }
 
-
+    public List<Order> getOrders() {
+        return orders;
+    }
 }
