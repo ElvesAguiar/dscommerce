6e2cec48e5c63723a3bc4afcdfd772a3991b130e
Elves Aguiar <elvesmhs@hotmail.com>
2023-05-17 18:07:28 -0300

    feat: ExceptionHandler

--- src/main/java/com/elves/dscommerce/services/ProductService.java
+++ src/main/java/com/elves/dscommerce/services/ProductService.java
@@ -3,6 +3,7 @@ package com.elves.dscommerce.services;
 import com.elves.dscommerce.dto.ProductDTO;
 import com.elves.dscommerce.entities.Product;
 import com.elves.dscommerce.repositories.ProductRepository;
+import com.elves.dscommerce.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,9 +18,12 @@ public class ProductService {
 
     @Autowired
     private ProductRepository repository;
+
     @Transactional(readOnly = true)
     public ProductDTO findById(Long id){
-        return new ProductDTO(repository.findById(id).get());
+
+
+        return new ProductDTO(repository.findById(id).orElseThrow(()->new ResourceNotFoundException("Recurso não encontrado!")));
     }
 
     @Transactional(readOnly = true)
