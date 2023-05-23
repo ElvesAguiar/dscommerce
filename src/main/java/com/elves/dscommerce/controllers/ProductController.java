30e559256eaeee3334e7b8a06fab7dc89a01dfc8
Elves Aguiar <elvesmhs@hotmail.com>
2023-05-23 17:04:06 -0300

    refactor: findAll method with custom params

--- src/main/java/com/elves/dscommerce/controllers/ProductController.java
+++ src/main/java/com/elves/dscommerce/controllers/ProductController.java
@@ -32,8 +32,10 @@ public class ProductController {
         }
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll (Pageable pageable){
-        Page<ProductDTO> page = service.findAll(pageable);
+    public ResponseEntity<Page<ProductDTO>> findAll (
+            @RequestParam(name = "name", defaultValue = "") String name,
+            Pageable pageable){
+        Page<ProductDTO> page = service.findAll(name,pageable);
         return ResponseEntity.ok(page);
     }
 
