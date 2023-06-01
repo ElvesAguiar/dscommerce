8719d47fdabd9e08290c4eac74497f9a5f6a01fe
Elves Aguiar <elvesmhs@hotmail.com>
2023-06-01 17:34:11 -0300

    feat: password encoder

--- src/main/java/com/elves/dscommerce/DscommerceApplication.java
+++ src/main/java/com/elves/dscommerce/DscommerceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DscommerceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DscommerceApplication.class, args);
-	}
 
+    public static void main(String[] args) {
+        SpringApplication.run(DscommerceApplication.class, args);
+    }
 }
--- src/main/java/com/elves/dscommerce/config/SecurityConfig.java
+++ src/main/java/com/elves/dscommerce/config/SecurityConfig.java
@@ -1,2 +1,25 @@
-package com.elves.dscommerce.config;public class SecurityConfig {
+package com.elves.dscommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable());
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        return http.build();
+    }
+
 }
