3e09ac88c554155444cebf0d44eda578f34ba5c4
Elves Aguiar <elvesmhs@hotmail.com>
2023-06-01 17:18:14 -0300

    feat: Entities Role with user relationship

--- src/main/java/com/elves/dscommerce/entities/User.java
+++ src/main/java/com/elves/dscommerce/entities/User.java
@@ -3,9 +3,7 @@ package com.elves.dscommerce.entities;
 import jakarta.persistence.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -15,7 +13,6 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-
     @Column(unique = true)
     private String email;
     private String phone;
@@ -24,6 +21,12 @@ public class User {
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User (){
 
@@ -88,6 +91,19 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        for (Role role : roles) {
+            if (role.getAuthority().equals(roleName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
