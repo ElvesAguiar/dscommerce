a9a8292594bb460239b2e88c0723817f21f91b1c
Elves Aguiar <elvesmhs@hotmail.com>
2023-06-04 13:51:40 -0300

    feat: authorization in routing

--- src/main/java/com/elves/dscommerce/controllers/ProductController.java
+++ src/main/java/com/elves/dscommerce/controllers/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -25,6 +26,7 @@ public class ProductController {
     @Autowired
     private ProductService service;
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
     @GetMapping(value = "/{id}")
         public ResponseEntity<ProductDTO> findById (@PathVariable Long id){
         ProductDTO dto = service.findById(id);
@@ -39,6 +41,7 @@ public class ProductController {
         return ResponseEntity.ok(page);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @PostMapping
     public ResponseEntity<ProductDTO>  insert(@Valid @RequestBody ProductDTO dto){
         dto= service.inset(dto);
@@ -48,12 +51,14 @@ public class ProductController {
         return ResponseEntity.created(uri).body(dto);
     }
 
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OPERATOR')")
     @PutMapping(value = "/{id}")
     public ResponseEntity<ProductDTO> update(@PathVariable Long id,@Valid @RequestBody ProductDTO dto){
         dto = service.update(id,dto);
         return ResponseEntity.ok(dto);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id){
         service.delete(id);
