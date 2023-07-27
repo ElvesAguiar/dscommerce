8b6e33a2b7b9b3d43f364545b7cf4372c584e584
Elves Aguiar <elvesmhs@hotmail.com>
2023-07-27 19:44:06 -0300

    feat: full coverage test coverage for Product Service

--- src/test/java/com/elves/dscommerce/services/ProductServiceTests.java
+++ src/test/java/com/elves/dscommerce/services/ProductServiceTests.java
@@ -6,16 +6,15 @@ import com.elves.dscommerce.entities.Product;
 import com.elves.dscommerce.repositories.ProductRepository;
 import com.elves.dscommerce.services.exceptions.ResourceNotFoundException;
 import com.elves.dscommerce.tests.ProductFactory;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.params.provider.Arguments;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -37,6 +36,7 @@ public class ProductServiceTests {
 
     private long existingProductId;
     private long nonExistingProductId;
+    private long referencedProductId;
 
     private String productName;
     private Product product;
@@ -49,18 +49,19 @@ public class ProductServiceTests {
     @BeforeEach
     void setUp() throws Exception {
         existingProductId = 1l;
+        referencedProductId = 2l;
         nonExistingProductId = 1000L;
 
         productName = "PS5";
         product = ProductFactory.createProduct(productName);
 
-        list= new ArrayList<>();
+        list = new ArrayList<>();
 
         list.add(product);
         list.add(ProductFactory.createProduct("PS5-Plus"));
 
         pageable = PageRequest.of(0, 12);
-        page= new PageImpl<>(list,pageable,pageable.getPageNumber());
+        page = new PageImpl<>(list, pageable, pageable.getPageNumber());
 
 
         Mockito.when(repository.findById(existingProductId)).thenReturn(Optional.of(product));
@@ -69,7 +70,8 @@ public class ProductServiceTests {
         Mockito.when(repository.save(ArgumentMatchers.any())).thenReturn(product);
         Mockito.when(repository.getReferenceById(existingProductId)).thenReturn(product);
         Mockito.when(repository.getReferenceById(nonExistingProductId)).thenThrow(ResourceNotFoundException.class);
-
+        Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(referencedProductId);
+        Mockito.doNothing().when(repository).deleteById(existingProductId);
 
     }
 
@@ -92,20 +94,20 @@ public class ProductServiceTests {
 
     @Test
     public void findAllShouldReturnPagedProductMinDTO() {
-        Pageable pageable1= PageRequest.of(0,12);
+        Pageable pageable1 = PageRequest.of(0, 12);
         String name = "PS5";
 
-        Page<ProductMinDTO> result=service.findAll(name,pageable1);
+        Page<ProductMinDTO> result = service.findAll(name, pageable1);
 
         Assertions.assertNotNull(result);
-        Assertions.assertEquals(result.get().toList().get(0).getName(),product.getName());
-        Assertions.assertEquals(result.stream().toList().get(1).getName(),"PS5-Plus");
+        Assertions.assertEquals(result.get().toList().get(0).getName(), product.getName());
+        Assertions.assertEquals(result.stream().toList().get(1).getName(), "PS5-Plus");
 
     }
 
     @Test
-    public void insertShouldReturnProductDTO(){
-        ProductService serviceSpy =Mockito.spy(service);
+    public void insertShouldReturnProductDTO() {
+        ProductService serviceSpy = Mockito.spy(service);
         ProductDTO result = serviceSpy.inset(new ProductDTO(product));
 
 
@@ -115,9 +117,9 @@ public class ProductServiceTests {
     }
 
     @Test
-    public void updateShouldReturnProductDTOWhenIdExists(){
-        ProductService serviceSpy =Mockito.spy(service);
-        ProductDTO result = serviceSpy.update(existingProductId,new ProductDTO(product));
+    public void updateShouldReturnProductDTOWhenIdExists() {
+        ProductService serviceSpy = Mockito.spy(service);
+        ProductDTO result = serviceSpy.update(existingProductId, new ProductDTO(product));
 
         Assertions.assertNotNull(result);
         Assertions.assertEquals(result.getName(), product.getName());
@@ -125,15 +127,33 @@ public class ProductServiceTests {
     }
 
     @Test
-    public void updateShouldReturnResourceNotFoundExceptionWhenIdExists(){
-        ProductService serviceSpy =Mockito.spy(service);
+    public void updateShouldReturnResourceNotFoundExceptionWhenIdExists() {
+        ProductService serviceSpy = Mockito.spy(service);
 
-        Assertions.assertThrows(ResourceNotFoundException.class,()->{
-            ProductDTO result = serviceSpy.update(nonExistingProductId,new ProductDTO(product));
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+            ProductDTO result = serviceSpy.update(nonExistingProductId, new ProductDTO(product));
 
         });
 
 
+    }
+
+    @Test
+    public void deleteShouldReturnVoidWhenExistingId() {
+
+        Assertions.assertDoesNotThrow(() -> {
+            service.delete(existingProductId);
+        });
+        Mockito.verify(repository, Mockito.times(1)).deleteById(existingProductId);
+    }
+
+    @Test
+    public void deleteShouldReturnDataIntegrityViolationExceptionWhenReferencedId() {
+
+        Assertions.assertThrows(DataIntegrityViolationException.class, () -> {
+            service.delete(referencedProductId);
+        });
+        Mockito.verify(repository, Mockito.times(1)).deleteById(referencedProductId);
     }
 
 }
