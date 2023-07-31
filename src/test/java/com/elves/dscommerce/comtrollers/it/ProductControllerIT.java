fc051e65930a44fbc802fb82eff64b1ff47b687a
Elves Aguiar <elvesmhs@hotmail.com>
2023-07-31 16:36:16 -0300

    feat: findAlÃl test in ProductControllerIT

---
+++ src/test/java/com/elves/dscommerce/comtrollers/it/ProductControllerIT.java
@@ -0,0 +1,67 @@
+package com.elves.dscommerce.comtrollers.it;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private String productName;
+
+    @BeforeEach
+    void setUp() {
+        productName = "mac";
+
+
+    }
+
+    @Test
+    public void findAllShouldReturnPageWhenNameParamIsNotEmpty() throws Exception {
+        ResultActions result = mockMvc
+                .perform(get("/products?name={productName}", productName)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.content[0].id").value(3L));
+        result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+        result.andExpect(jsonPath("$.content[0].price").value(1250.0));
+        result.andExpect(jsonPath("$.content[0].imgUrl")
+                .value("https://raw.githubusercontent.com/devsuperior" +
+                       "/dscatalog-resources/master/backend/img/3-big.jpg"));
+    }
+
+    @Test
+    public void findAllShouldReturnPageWhenNameParamIsEmpty() throws Exception {
+        ResultActions result = mockMvc
+                .perform(get("/products")
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$.content[0].id").value(1L));
+        result.andExpect(jsonPath("$.content[0].name").value("The Lord of the Rings"));
+        result.andExpect(jsonPath("$.content[0].price").value(90.5));
+        result.andExpect(jsonPath("$.content[0].imgUrl")
+                .value("https://raw.githubusercontent.com/devsuperior" +
+                       "/dscatalog-resources/master/backend/img/1-big.jpg"));
+    }
+
+//    @Test
+//    public void insertShouldReturnProductWhen
+
+}
